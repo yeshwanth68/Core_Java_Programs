04ad5232c1644bdab018fd5fdcacca2af95ba480
yeshwanth68 <yeshwanthkumar200@gmail.com>
2024-02-24 19:58:21 +0530

    Fibonacci

---
+++ java/Programs/FibonacciNumber.java
@@ -0,0 +1,27 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int p=0;
+		int i=1;
+		int count = 2;
+		
+		while(count<=n)
+		{
+			int temp=i;
+			i=i+p;
+			p=temp;
+			count++;
+		}
+		System.out.println("Value="+i);
+		
+
+	}
+
+}
